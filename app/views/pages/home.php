b41ee920fe3f3271602abbbfa6caba8d8472cc52
mishevski-k <mishevski0800@gmail.com>
2021-09-25 15:49:22 +0200

    Initial commit

---
+++ app/views/pages/home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/home.css">
+
+    <title><?php echo SITENAME ?></title>
+</head>
+<body>
+    <div class="container">
+        <div class="banner">
+            <h1>Lumos<span>PHP</span></h1>
+            <p>For better development</p>
+        </div>
+        <div class="changelog-container">
+            <h1>Changelog</h1>
+            <div class="changelogs">
+                <h2 class="changelog">9/5/2021: Nothing to show . . .</h2>
+                <h2 class="changelog">9/13/2021: Added basic styling configurations . . . </h2>
+            </div>
+        </div>
+        <p id="version">Version 1.0</p>
+    </div>   
+</body>
+</html>
